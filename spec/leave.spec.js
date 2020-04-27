bdc2b6d5916cb7aa8d78da6802d1692e09515c7b
Iman Setya A <imansaikis@gmail.com>
2020-04-27 13:51:16 +0700

    fix(spec): add unit test for leave function

--- spec/leave.spec.js
+++ spec/leave.spec.js
@@ -4,10 +4,24 @@ describe("check leave function", () =>{
     afterEach(()=>{
         parking.resetParking();
     });
-    it("Empty  slot input", ()=>{
+    it("Empty parameter", ()=>{
+        expect(parking.leave()).toBe("Registry No. cannot be empty")
+    });
+    it("Empty regitry input", ()=>{
         expect(parking.leave("",10)).toBe("Registry No. cannot be empty")
     });
-    it("Empty hour input", ()=>{
-        expect(parking.leave(10,"")).not.toBe("hour must be a number")
+    it("Escape sequence registy input", ()=>{
+        expect(parking.leave("\n",10)).toBe("Registry No. cannot be empty")
     });
+
+    it("Empty string hour input", ()=>{
+        expect(parking.leave("KA-01-HH-1231","")).toBe("hour must be a number")
+    });
+    it("Empty parameter hour input", ()=>{
+        expect(parking.leave("KA-01-HH-1231")).toBe("hour must be a number")
+    });
+    it("Escape sequence hour input", ()=>{
+        expect(parking.leave("KA-01-HH-1231","\n")).toBe("hour must be a number")
+    });
+    
 })
